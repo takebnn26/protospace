26f28be612662a379470907902951492e8e7e0f3
隼人 <takebnn26@gmail.com>
2016-09-15 16:49:10 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :email, presence: true
+  validates :name, presence: true, length: { maximum: 20}
+  validates :group, presence: true
+  validates :profile, presence: true
+  validates :works, presence: true
 end
